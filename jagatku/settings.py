a04dec689066d2d9145d886b22468dd688310a9e
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2025-06-25 23:22:29 +0700

    startapp slang

--- jagatku/settings.py
+++ jagatku/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'slang.apps.SlangConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ slang/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, slang index page is under construction.")
