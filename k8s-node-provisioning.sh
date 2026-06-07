fd6f32ddda41e1d6b13fc6fd8c2bf5ca779d24f1
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-06-07 17:48:10 +0700

    install module and configure kernel

--- k8s-node-provisioning.sh
+++ k8s-node-provisioning.sh
@@ -3,3 +3,21 @@
 # Disable swap memory permanently
 sudo sed -i '/swap/d' /etc/fstab
 sudo swapoff -a
+
+# Load kubernetes kernel modules
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+    overlay
+    br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Configure sysctl networking
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+    net.bridge.bridge-nf-call-ip6tables = 1
+    net.bridge.bridge-nf-call-iptables = 1
+    net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
