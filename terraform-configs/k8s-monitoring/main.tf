e7aa76e4254c69dc0fd70de4e15cd58e5cb6b4d2
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-06-29 02:14:34 +0700

    rename k8s-monitoring folder

---
+++ terraform-configs/k8s-monitoring/main.tf
@@ -0,0 +1,14 @@
+resource "kubernetes_namespace_v1" "observability" {
+  metadata {
+    name = "observability"
+  }
+}
+
+resource "helm_release" "k8s_monitoring" {
+  name       = "k8s-monitoring"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "k8s-monitoring"
+  namespace  = kubernetes_namespace_v1.observability.metadata[0].name
+  version    = "4.1.6"
+#   values = []
+}
