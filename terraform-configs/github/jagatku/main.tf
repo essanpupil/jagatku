f301064cf7600a18c28fb2cd27db9cad0bbfaf58
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-04 22:53:27 +0700

    add default argument

--- terraform-configs/github/jagatku/main.tf
+++ terraform-configs/github/jagatku/main.tf
@@ -3,6 +3,7 @@ resource "github_repository" "jagatku" {
   allow_merge_commit = false
   allow_rebase_merge = false
   allow_squash_merge = false
+  has_downloads      = true
   has_issues         = true
   has_projects       = true
   has_wiki           = true
