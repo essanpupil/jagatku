0f875c82ada16b030f0cf823132492884d5b3710
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-08 23:47:09 +0700

    sync datasource code with existing settings

--- terraform-configs/grafana/datasource/loki-local/main.tf
+++ terraform-configs/grafana/datasource/loki-local/main.tf
@@ -1,9 +1,16 @@
 resource "grafana_data_source" "this" {
-  type                = "loki"
-  name                = "Loki-Local"
+  type = "loki"
+  name = "Loki-Local"
+  url  = "http://localhost:3100"
+  json_data_encoded = jsonencode(
+    {
+      tlsAuth           = false
+      tlsAuthWithCACert = false
+    }
+  )
 }
 
 import {
-    to = grafana_data_source.this
-    id = "afqh58e2dhm9sc"
+  to = grafana_data_source.this
+  id = "afqh58e2dhm9sc"
 }
--- terraform-configs/grafana/datasource/prometheus-local/main.tf
+++ terraform-configs/grafana/datasource/prometheus-local/main.tf
@@ -1,9 +1,16 @@
 resource "grafana_data_source" "this" {
-  type                = "prometheus"
-  name                = "Prometheus-Local"
+  type = "prometheus"
+  name = "Prometheus-Local"
+  url  = "http://localhost:9090"
+  json_data_encoded = jsonencode(
+    {
+      tlsAuth           = false
+      tlsAuthWithCACert = false
+    }
+  )
 }
 
 import {
-    to = grafana_data_source.this
-    id = "dfqgw3rsao6bkf"
+  to = grafana_data_source.this
+  id = "dfqgw3rsao6bkf"
 }
