d7b0b372c864039c1ab40f1c04e77e5cf596e9ae
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-08 17:55:02 +0700

    use dedicated values file

--- terraform-configs/kubernetes/k8s-monitoring/main.tf
+++ terraform-configs/kubernetes/k8s-monitoring/main.tf
@@ -11,44 +11,6 @@ resource "helm_release" "k8s_monitoring" {
   namespace  = kubernetes_namespace_v1.observability.metadata[0].name
   version    = "4.1.6"
   values = [
-    <<-EOF
-    collectors:
-      alloy-metrics:
-        enabled: true
-        presets: [medium, clustered, statefulset]
-
-      alloy-logs:
-        enabled: true
-        presets: [small, filesystem-log-reader, daemonset]
-
-      alloy-singleton:
-        presets: [small, deployment]
-
-    clusterEvents:
-      enabled: true
-      collector: alloy-singleton
-    nodeLogs:
-      enabled: true
-      collector: alloy-logs
-    hostMetrics:
-      enabled: true
-      collector: alloy-metrics
-    
-    cluster:
-      name: "kubernetes"
-    destinations:
-      localPrometheus:
-        type: prometheus
-        url: http://192.168.1.2:9090
-
-      hostedLogs:
-        type: loki
-        url: http://192.168.1.2:3100/loki/api/v1/push
-        auth:
-          # type: basic
-          # username: "my-username"
-          # password: "my-password"
-          tenantId: "jagatku"
-    EOF
+    file("${path.module}/values.yaml")
   ]
 }
