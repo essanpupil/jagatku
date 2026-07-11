b876d63b59a863e4a99ae7a42ea9dc3b33b7c042
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-11 14:27:58 +0700

    update chart reference

--- terraform-configs/kubernetes/cloudnative-pg/main.tf
+++ terraform-configs/kubernetes/cloudnative-pg/main.tf
@@ -7,7 +7,7 @@ resource "kubernetes_namespace_v1" "this" {
 resource "helm_release" "this" {
   name       = "cloudnative-pg"
   repository = "helm repo add cnpg https://cloudnative-pg.github.io/charts"
-  chart      = "cloudnative-pg/cloudnative-pg"
+  chart      = "cloudnative-pg"
   namespace  = kubernetes_namespace_v1.this.metadata[0].name
   version    = "0.29.0"
   # atomic     = true
