b7d6021a3ad25062c8e24b4c32f7029a4df29f4b
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-12 04:53:15 +0700

    clean up on failure but do not wait tis finish

--- terraform-configs/kubernetes/kafka/main.tf
+++ terraform-configs/kubernetes/kafka/main.tf
@@ -5,13 +5,16 @@ resource "kubernetes_namespace_v1" "this" {
 }
 
 resource "helm_release" "this" {
-  name       = "kafka"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "kafka"
-  namespace  = kubernetes_namespace_v1.this.metadata[0].name
-  version    = "32.4.3"
-  # atomic     = true
-  # wait       = true
+  name            = "kafka"
+  repository      = "https://charts.bitnami.com/bitnami"
+  chart           = "kafka"
+  namespace       = kubernetes_namespace_v1.this.metadata[0].name
+  version         = "32.4.3"
+  cleanup_on_fail = true
+  atomic          = true
+
+  # Prevents Terraform timeouts if a chart has complex post-install jobs
+  wait = false
   values = [
     file("${path.module}/values.yaml")
   ]
