0ea1ad63bf2aba6bc33761e53a2048a271a1b6e4
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-13 01:50:08 +0700

    rerun nfs ganesha deletion

--- terraform-configs/kubernetes/nfs-ganesha/main.tf
+++ terraform-configs/kubernetes/nfs-ganesha/main.tf
@@ -3,11 +3,9 @@
 #   repository = "https://kubernetes-sigs.github.io/nfs-ganesha-server-and-external-provisioner/"
 #   chart      = "nfs-server-provisioner"
 #   namespace  = "kube-system"
-#   # version         = "32.4.3"
-#   # cleanup_on_fail = true
-#   # atomic          = true
+#   cleanup_on_fail = true
+#   atomic          = true
 
-#   # Prevents Terraform timeouts if a chart has complex post-install jobs
 #   wait = false
 #   values = [
 #     file("${path.module}/values.yaml")
