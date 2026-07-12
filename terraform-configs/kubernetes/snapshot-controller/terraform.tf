acf1a7c303c0ffb2812026eb5352cbb974098893
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-12 21:57:07 +0700

    install csi snapshotter required for longhorn snapshot volume

---
+++ terraform-configs/kubernetes/snapshot-controller/terraform.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "consul" {
+    address = "192.168.1.2:8500"
+    scheme  = "http"
+    path    = "terraform-configs/kubernetes/snapshot-controller"
+  }
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.2.0"
+    }
+  }
+}
+
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
