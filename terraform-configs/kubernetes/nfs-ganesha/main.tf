438f9faab715e08a21769b96f37676ef2b27f4a6
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-12 15:57:19 +0700

    remove testing nfs storage class

--- terraform-configs/kubernetes/nfs-ganesha/main.tf
+++ terraform-configs/kubernetes/nfs-ganesha/main.tf
@@ -1,15 +1,15 @@
-resource "helm_release" "this" {
-  name       = "nfs-ganesha"
-  repository = "https://kubernetes-sigs.github.io/nfs-ganesha-server-and-external-provisioner/"
-  chart      = "nfs-server-provisioner"
-  namespace  = "kube-system"
-  # version         = "32.4.3"
-  # cleanup_on_fail = true
-  # atomic          = true
+# resource "helm_release" "this" {
+#   name       = "nfs-ganesha"
+#   repository = "https://kubernetes-sigs.github.io/nfs-ganesha-server-and-external-provisioner/"
+#   chart      = "nfs-server-provisioner"
+#   namespace  = "kube-system"
+#   # version         = "32.4.3"
+#   # cleanup_on_fail = true
+#   # atomic          = true
 
-  # Prevents Terraform timeouts if a chart has complex post-install jobs
-  wait = false
-  values = [
-    file("${path.module}/values.yaml")
-  ]
-}
+#   # Prevents Terraform timeouts if a chart has complex post-install jobs
+#   wait = false
+#   values = [
+#     file("${path.module}/values.yaml")
+#   ]
+# }
