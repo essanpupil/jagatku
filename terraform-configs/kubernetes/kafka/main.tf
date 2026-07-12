c19ce5f349bc78dd05ac36e7fccd7fb671653e79
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-12 22:25:14 +0700

    reformat terraform

--- terraform-configs/kubernetes/kafka/main.tf
+++ terraform-configs/kubernetes/kafka/main.tf
@@ -5,14 +5,10 @@ resource "kubernetes_namespace_v1" "this" {
 }
 
 resource "kubernetes_manifest" "strimzi" {
-  manifest = yamldecode(
-    file("${path.module}/strimzi.yaml")
-  )
+  manifest = yamldecode(file("${path.module}/strimzi.yaml"))
 }
 
 resource "kubernetes_manifest" "kafka" {
   depends_on = [kubernetes_manifest.strimzi]
-  manifest = yamldecode(
-    file("${path.module}/kafka-single-node.yaml")
-  )
+  manifest   = yamldecode(file("${path.module}/kafka-single-node.yaml"))
 }
