c8f3d60f87b740b6f74f1ddd38dbe85f468742a1
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-14 23:51:54 +0700

    uninstall kafka to reduce cluster load for now

--- terraform-configs/kubernetes/kafka/main.tf
+++ terraform-configs/kubernetes/kafka/main.tf
@@ -1,23 +1,23 @@
-resource "kubernetes_namespace_v1" "this" {
-  metadata {
-    name = "queue"
-  }
-}
+# resource "kubernetes_namespace_v1" "this" {
+#   metadata {
+#     name = "queue"
+#   }
+# }
 
-data "kubectl_path_documents" "strimzi" {
-  pattern = "${path.module}/strimzi.yaml"
-}
+# data "kubectl_path_documents" "strimzi" {
+#   pattern = "${path.module}/strimzi.yaml"
+# }
 
-resource "kubectl_manifest" "strimzi" {
-  for_each  = toset(data.kubectl_path_documents.strimzi.documents)
-  yaml_body = each.value
-}
+# resource "kubectl_manifest" "strimzi" {
+#   for_each  = toset(data.kubectl_path_documents.strimzi.documents)
+#   yaml_body = each.value
+# }
 
-data "kubectl_path_documents" "kafka" {
-  pattern = "${path.module}/kafka-single-node.yaml"
-}
+# data "kubectl_path_documents" "kafka" {
+#   pattern = "${path.module}/kafka-single-node.yaml"
+# }
 
-resource "kubectl_manifest" "kafka" {
-  for_each  = toset(data.kubectl_path_documents.kafka.documents)
-  yaml_body = each.value
-}
+# resource "kubectl_manifest" "kafka" {
+#   for_each  = toset(data.kubectl_path_documents.kafka.documents)
+#   yaml_body = each.value
+# }
