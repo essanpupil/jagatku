010d40f7f39b758eff59aeed09419cf97d1cdc0b
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-14 23:34:00 +0700

    remove grafana dashboards and folder created by terraform
    Will use grafana git-sync for dashboards

--- terraform-configs/grafana/dashboards/kube-events/main.tf
+++ terraform-configs/grafana/dashboards/kube-events/main.tf
@@ -1,9 +1,9 @@
-resource "grafana_folder" "this" {
-  title = "Kubernetes Critical Events"
-  uid   = "kubernetes"
-}
+# resource "grafana_folder" "this" {
+#   title = "Kubernetes Critical Events"
+#   uid   = "kubernetes"
+# }
 
-resource "grafana_dashboard" "this" {
-  folder      = grafana_folder.this.uid
-  config_json = file("${path.module}/kubernetes-dashboard.json")
-}
+# resource "grafana_dashboard" "this" {
+#   folder      = grafana_folder.this.uid
+#   config_json = file("${path.module}/kubernetes-dashboard.json")
+# }
