5059ac20460fa45a9d44afbf4232643347a54e58
Ikhsan N. Rosyidin <ikhsan.rosyidin@gmail.com>
2026-07-21 18:55:46 +0700

    fix double quote miss

--- terraform-configs/kubernetes/cillium/main.tf
+++ terraform-configs/kubernetes/cillium/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "this" {
   name       = "cillium"
   repository = "https://helm.cilium.io/"
   chart      = "cillium"
-  namespace  = "kube-system
+  namespace  = "kube-system"
   version    = "1.19.6"
   # atomic     = true
   # wait       = true
