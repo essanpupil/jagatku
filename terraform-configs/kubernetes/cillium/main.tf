9b150dac1b1159764d899edb3381cf2dee89d0bb
Ikhsan N. Rosyidin <ikhsan.rosyidin@gmail.com>
2026-07-21 18:53:32 +0700

    redeploy cillium using terraform

---
+++ terraform-configs/kubernetes/cillium/main.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "this" {
+  name       = "cillium"
+  repository = "https://helm.cilium.io/"
+  chart      = "cillium"
+  namespace  = "kube-system
+  version    = "1.19.6"
+  # atomic     = true
+  # wait       = true
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
