b1325924bb2a9afa08b4b0338090de26d1fe6028
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-28 00:17:33 +0700

    redeploy traefik

--- terraform-configs/kubernetes/traefik/main.tf
+++ terraform-configs/kubernetes/traefik/main.tf
@@ -10,8 +10,8 @@ resource "helm_release" "this" {
   chart      = "traefik"
   namespace  = kubernetes_namespace_v1.this.metadata[0].name
   version    = "41.0.2"
-  atomic     = true
-  wait       = true
+  # atomic     = true
+  wait = true
   values = [
     file("${path.module}/values.yaml")
   ]
