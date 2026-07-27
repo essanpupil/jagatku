d95d26d44be2d05f84ea959e7c76b3c2b0b91345
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-27 23:52:13 +0700

    use latest version of traefik

--- terraform-configs/kubernetes/traefik/main.tf
+++ terraform-configs/kubernetes/traefik/main.tf
@@ -9,7 +9,7 @@ resource "helm_release" "this" {
   repository = "https://traefik.github.io/charts"
   chart      = "traefik"
   namespace  = kubernetes_namespace_v1.this.metadata[0].name
-  version    = "9.2.0"
+  version    = "41.0.2"
   atomic     = true
   wait       = true
   values = [
