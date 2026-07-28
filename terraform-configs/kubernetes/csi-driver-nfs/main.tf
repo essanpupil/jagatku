7b61e1014080ddc619b9938d4069462e20b52b6f
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-28 15:42:32 +0700

    refert back repo path and usel latest helm release version

--- terraform-configs/kubernetes/csi-driver-nfs/main.tf
+++ terraform-configs/kubernetes/csi-driver-nfs/main.tf
@@ -1,9 +1,9 @@
 resource "helm_release" "this" {
   name       = "csi-driver-nfs"
-  repository = "https://kubernetes-csi.github.io"
+  repository = "https://kubernetes-csi.github.io/csi-driver-nfs"
   chart      = "csi-driver-nfs"
   namespace  = "kube-system"
-  version    = "4.9.0"
+  version    = "4.13.4"
   # atomic     = true
   # wait       = true
   values = [
