86af9eed710bbae59c72ffa02279d0dd35ddf489
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-28 15:39:19 +0700

    fix repo path

--- terraform-configs/kubernetes/csi-driver-nfs/main.tf
+++ terraform-configs/kubernetes/csi-driver-nfs/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "this" {
   name       = "csi-driver-nfs"
-  repository = "https://kubernetes-csi.github.io/csi-driver-nfs"
+  repository = "https://kubernetes-csi.github.io"
   chart      = "csi-driver-nfs"
   namespace  = "kube-system"
   version    = "4.9.0"
