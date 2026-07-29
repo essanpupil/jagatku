2312500250b8ce8314065a44603b944085f62117
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-29 13:29:28 +0700

    deploy alloy singleton

--- terraform-configs/kubernetes/k8s-monitoring/main.tf
+++ terraform-configs/kubernetes/k8s-monitoring/main.tf
@@ -1,18 +1,22 @@
-resource "kubernetes_namespace_v1" "observability" {
-  metadata {
-    name = "observability"
+data "terraform_remote_state" "observability_namespace" {
+  backend = "consul"
+
+  config = {
+    address = "192.168.1.2:8500"
+    scheme  = "http"
+    path    = "terraform-configs/kubernetes/prometheus-agent"
   }
 }
 
-# resource "helm_release" "k8s_monitoring" {
-#   name       = "k8s-monitoring"
-#   repository = "https://grafana.github.io/helm-charts"
-#   chart      = "k8s-monitoring"
-#   namespace  = kubernetes_namespace_v1.observability.metadata[0].name
-#   version    = "4.1.6"
-#   atomic     = true
-#   wait       = true
-#   values = [
-#     file("${path.module}/values.yaml")
-#   ]
-# }
+resource "helm_release" "k8s_monitoring" {
+  name       = "k8s-monitoring"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "k8s-monitoring"
+  namespace  = data.terraform_remote_state.observability_namespace.outputs.namespace
+  version    = "4.1.6"
+  atomic     = true
+  wait       = true
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
