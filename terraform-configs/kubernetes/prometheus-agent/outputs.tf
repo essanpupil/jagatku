6d746c0aa6cb1299604d2105767625c8c149465f
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-29 14:40:46 +0700

    remove prometheus agent because will focus observability agents by alloy

--- terraform-configs/kubernetes/prometheus-agent/outputs.tf
+++ terraform-configs/kubernetes/prometheus-agent/outputs.tf
@@ -1,4 +1,4 @@
-output "namespace" {
-  value       = kubernetes_namespace_v1.this.metadata[0].name
-  description = "Created namespace"
-}
+# output "namespace" {
+#   value       = kubernetes_namespace_v1.this.metadata[0].name
+#   description = "Created namespace"
+# }
