9a2563381f2b2229d300e5a091c5faa310c7b02e
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-29 17:42:17 +0700

    deploy alloy monitoring cluster metrics

--- terraform-configs/kubernetes/k8s-monitoring/main.tf
+++ terraform-configs/kubernetes/k8s-monitoring/main.tf
@@ -1,10 +1,6 @@
-data "terraform_remote_state" "observability_namespace" {
-  backend = "consul"
-
-  config = {
-    address = "192.168.1.2:8500"
-    scheme  = "http"
-    path    = "terraform-configs/kubernetes/prometheus-agent"
+resource "kubernetes_namespace_v1" "this" {
+  metadata {
+    name = "observability"
   }
 }
 
@@ -12,7 +8,7 @@ resource "helm_release" "k8s_monitoring" {
   name       = "k8s-monitoring"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "k8s-monitoring"
-  namespace  = data.terraform_remote_state.observability_namespace.outputs.namespace
+  namespace  = kubernetes_namespace_v1.this.metadata[0].name
   version    = "4.1.6"
   atomic     = true
   wait       = true
