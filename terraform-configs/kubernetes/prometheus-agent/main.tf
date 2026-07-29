4c032f1c6964d81ce3c49088e8201eaf7762cbba
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-29 13:00:47 +0700

    deploy kube state metrics with namespace

--- terraform-configs/kubernetes/prometheus-agent/main.tf
+++ terraform-configs/kubernetes/prometheus-agent/main.tf
@@ -1,10 +1,6 @@
-data "terraform_remote_state" "observability_namespace" {
-  backend = "consul"
-
-  config = {
-    address = "192.168.1.2:8500"
-    scheme  = "http"
-    path    = "terraform-configs/kubernetes/k8s-monitoring"
+resource "kubernetes_namespace_v1" "this" {
+  metadata {
+    name = "observability"
   }
 }
 
@@ -12,7 +8,7 @@ resource "helm_release" "this" {
   name       = "kube-prometheus-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  namespace  = data.terraform_remote_state.observability_namespace.outputs.namespace
+  namespace  = kubernetes_namespace_v1.this.metadata[0].name
   version    = "87.15.1"
   # atomic     = true
   # wait       = true
