c8764fa3b4beca4da7f2a80fbc8aa5af55fda797
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-07-29 19:17:55 +0700

    K8s annotation discovery monitoring (#260)
    * enable annotation auto discovery in k8s-monotoring
    
    * set cilium deployment to atomic
    
    * set annotation scraping for cilium
    
    * set annotation scraping for traeefik

--- terraform-configs/kubernetes/cillium/main.tf
+++ terraform-configs/kubernetes/cillium/main.tf
@@ -4,8 +4,8 @@ resource "helm_release" "this" {
   chart      = "cilium"
   namespace  = "kube-system"
   version    = "1.19.6"
-  # atomic     = true
-  # wait       = true
+  atomic     = true
+  wait       = true
   values = [
     file("${path.module}/values.yaml")
   ]
