afd7917fb4741d54c64ad88419a1db496afb6194
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-06 01:54:39 +0700

    create namespace and deploy hashicorp vault

--- terraform-configs/kubernetes/hashicorp-vault/main.tf
+++ terraform-configs/kubernetes/hashicorp-vault/main.tf
@@ -1,12 +1,18 @@
-# resource "helm_release" "this" {
-#   name       = "vault"
-#   repository = "https://helm.releases.hashicorp.com"
-#   chart      = "vault"
-#   namespace  = "kube-system"
-#   version    = "0.34.0"
-#   atomic     = true
-#   wait       = true
-#   values = [
-#     file("${path.module}/values.yaml")
-#   ]
-# }
+resource "kubernetes_namespace_v1" "this" {
+  metadata {
+    name = "vault"
+  }
+}
+
+resource "helm_release" "this" {
+  name       = "vault"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+  namespace  = kubernetes_namespace_v1.this.metadata[0].name
+  version    = "0.34.0"
+  atomic     = true
+  wait       = true
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
