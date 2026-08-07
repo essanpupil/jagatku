9932e05024eb7cfcaf358379b58638f8cabbb4cc
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-07 20:34:02 +0700

    prepare network policy for core services

--- terraform-configs/kubernetes/network-policies/main.tf
+++ terraform-configs/kubernetes/network-policies/main.tf
@@ -1,4 +1,11 @@
-#trivy:ignore:KSV-0037
 resource "kubernetes_manifest" "coredns_apiserver" {
   manifest = yamldecode(file("${path.module}/coredns-apiserver.yaml"))
+}
+
+resource "kubernetes_manifest" "scheduler_apiserver" {
+  manifest = yamldecode(file("${path.module}/scheduler-apiserver.yaml"))
+}
+
+resource "kubernetes_manifest" "controller_manager_apiserver" {
+  manifest = yamldecode(file("${path.module}/controller-manager-apiserver.yaml"))
 }
