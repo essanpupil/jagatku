9636f969a1583ba91c3d0a390312bfcae619691e
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-07 22:45:56 +0700

    refactor alloy ingress policy to coredns (#323)
    * allow alloy-singleton access kube-dns and kube-apiserver
    
    * fine tune match labels to specify namespace
    
    * refactor alloy ingress policy to coredns
    
    * Merge branch 'main' into alloy-access-internodes
    
    * move network policy to dedicated config folder to avoid downloading helm chart during cicd

--- terraform-configs/kubernetes/network-policies/main.tf
+++ terraform-configs/kubernetes/network-policies/main.tf
@@ -2,6 +2,8 @@ resource "kubernetes_manifest" "coredns_apiserver" {
   manifest = yamldecode(file("${path.module}/coredns-apiserver.yaml"))
 }
 
-resource "kubernetes_manifest" "alloy_coredns" {
-  manifest = yamldecode(file("${path.module}/alloy-coredns.yaml"))
+resource "kubernetes_manifest" "alloy" {
+  manifest = yamldecode(templatefile("${path.module}/alloy-network-policy.yaml", {
+    namespace = data.terraform_remote_state.k8s_monitoring.outputs.namespace
+  }))
 }
