954ba3bab106ca5581f1ab8c694bd6f724f3a89b
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-07 22:14:30 +0700

    refactor alloy ingress policy to coredns

--- terraform-configs/kubernetes/network-policies/main.tf
+++ terraform-configs/kubernetes/network-policies/main.tf
@@ -1,7 +1,3 @@
 resource "kubernetes_manifest" "coredns_apiserver" {
   manifest = yamldecode(file("${path.module}/coredns-apiserver.yaml"))
 }
-
-resource "kubernetes_manifest" "alloy_coredns" {
-  manifest = yamldecode(file("${path.module}/alloy-coredns.yaml"))
-}
