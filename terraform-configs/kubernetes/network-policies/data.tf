5a5851f6493caa5fe42299b68a53de36013e76a1
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-07 23:33:05 +0700

    fix remote state path

--- terraform-configs/kubernetes/network-policies/data.tf
+++ terraform-configs/kubernetes/network-policies/data.tf
@@ -2,6 +2,6 @@ data "terraform_remote_state" "k8s_monitoring" {
   backend = "consul"
   config = {
     address = "consul.laptop1.local"
-    path    = "terraform-configs/kubernetes/cillium"
+    path    = "terraform-configs/kubernetes/k8s-monitoring"
   }
 }
