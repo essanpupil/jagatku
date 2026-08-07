c46ba37d68b671696b7e0b2aa57a6a46ac56c1ac
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-07 20:51:40 +0700

    Alloy coredns networkpolicy (#322)
    * allow alloy-singleton access kube-dns and kube-apiserver
    
    * fine tune match labels to specify namespace

--- terraform-configs/kubernetes/network-policies/main.tf
+++ terraform-configs/kubernetes/network-policies/main.tf
@@ -1,4 +1,7 @@
-#trivy:ignore:KSV-0037
 resource "kubernetes_manifest" "coredns_apiserver" {
   manifest = yamldecode(file("${path.module}/coredns-apiserver.yaml"))
+}
+
+resource "kubernetes_manifest" "alloy_coredns" {
+  manifest = yamldecode(file("${path.module}/alloy-coredns.yaml"))
 }
