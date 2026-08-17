536c3250438f193893adafedb0d6dc1680514b32
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-17 15:49:01 +0700

    retrigger traefik uninstall

--- terraform-configs/kubernetes/traefik/main.tf
+++ terraform-configs/kubernetes/traefik/main.tf
@@ -9,7 +9,7 @@ resource "kubernetes_namespace_v1" "this" {
 #   repository = "https://traefik.github.io/charts"
 #   chart      = "traefik"
 #   namespace  = kubernetes_namespace_v1.this.metadata[0].name
-#   version    = "41.0.2"
+#   version    = "41.2.0"
 #   atomic     = true
 #   wait       = true
 #   values = [
