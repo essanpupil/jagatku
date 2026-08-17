575ff5059e54218224304cf011aa746d1a0b0b2e
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-17 15:17:30 +0700

    uninstall traefik for now

--- terraform-configs/kubernetes/traefik/main.tf
+++ terraform-configs/kubernetes/traefik/main.tf
@@ -4,15 +4,15 @@ resource "kubernetes_namespace_v1" "this" {
   }
 }
 
-resource "helm_release" "this" {
-  name       = "traefik"
-  repository = "https://traefik.github.io/charts"
-  chart      = "traefik"
-  namespace  = kubernetes_namespace_v1.this.metadata[0].name
-  version    = "41.0.2"
-  atomic     = true
-  wait       = true
-  values = [
-    file("${path.module}/values.yaml")
-  ]
-}
+# resource "helm_release" "this" {
+#   name       = "traefik"
+#   repository = "https://traefik.github.io/charts"
+#   chart      = "traefik"
+#   namespace  = kubernetes_namespace_v1.this.metadata[0].name
+#   version    = "41.0.2"
+#   atomic     = true
+#   wait       = true
+#   values = [
+#     file("${path.module}/values.yaml")
+#   ]
+# }
