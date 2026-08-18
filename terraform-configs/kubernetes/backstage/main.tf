cfc1a5597ba69e6c9b64a42183724812b42c48a9
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-18 10:43:42 +0700

    deploy spotify backstage

---
+++ terraform-configs/kubernetes/backstage/main.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_namespace_v1" "this" {
+  metadata {
+    name = "platform"
+  }
+}
+
+resource "helm_release" "this" {
+  name       = "backstage"
+  repository = "oci://ghcr.io/backstage/charts/backstage"
+  chart      = "backstage"
+  namespace  = kubernetes_namespace_v1.this.metadata[0].name
+  version    = "2.10.0"
+  #   atomic     = true
+  #   wait       = true
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
