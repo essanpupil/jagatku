99fca6eb435168c00384ed89753ce15a211d277a
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-18 10:51:41 +0700

    update helm repo url

--- terraform-configs/kubernetes/backstage/main.tf
+++ terraform-configs/kubernetes/backstage/main.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace_v1" "this" {
 
 resource "helm_release" "this" {
   name       = "backstage"
-  repository = "oci://ghcr.io/backstage/charts/backstage"
+  repository = "oci://ghcr.io/backstage/charts"
   chart      = "backstage"
   namespace  = kubernetes_namespace_v1.this.metadata[0].name
   version    = "2.10.0"
