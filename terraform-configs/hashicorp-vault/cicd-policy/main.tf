dcabfddc5796019cc2415f7fb25d35875bfced31
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-19 16:48:04 +0700

    refactor vault policy for cicd

--- terraform-configs/hashicorp-vault/cicd-policy/main.tf
+++ terraform-configs/hashicorp-vault/cicd-policy/main.tf
@@ -1,13 +1,20 @@
+data "vault_policy_document" "this" {
+  rule {
+    path         = "grafana-token/metadata/*"
+    capabilities = ["read", "list"]
+    # description  = "allow all on secrets"
+  }
+
+  rule {
+    path         = "grafana-token/data/*"
+    capabilities = ["create", "read", "update", "list"]
+    # description  = "allow all on secrets"
+  }
+}
+
 resource "vault_policy" "this" {
   name   = "laptop1"
-  policy = <<EOT
-path "grafana-token/metadata/*" {
-    capabilities = ["read", "list"]
-}
-path "grafana-token/data/*" {
-    capabilities = ["create", "read", "update", "list"]
-}
-EOT
+  policy = data.vault_policy_document.this.hcl
 }
 
 import {
