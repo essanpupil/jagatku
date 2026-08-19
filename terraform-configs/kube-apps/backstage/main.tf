176bb89b60c7c7e71fa58a83f3972c76f2ba7be2
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-20 02:02:48 +0700

    harcode path parameter

--- terraform-configs/kube-apps/backstage/main.tf
+++ terraform-configs/kube-apps/backstage/main.tf
@@ -41,12 +41,10 @@ resource "vault_kv_secret_v2" "backstage_config" {
   })
 }
 
-# resource "kubernetes_manifest" "db_secrets" {
-#   depends_on = [vault_kv_secret_v2.backstage_config]
-
-#   manifest = yamldecode(templatefile("${path.module}/secret-provider.yaml", {
-#     namespace   = kubernetes_namespace_v1.this.metadata[0].name
-#     vault_role  = vault_kubernetes_auth_backend_role.this.role_name
-#     secret_path = vault_kv_secret_v2.backstage_config.path
-#   }))
-# }
+resource "kubernetes_manifest" "db_secrets" {
+  manifest = yamldecode(templatefile("${path.module}/secret-provider.yaml", {
+    namespace   = kubernetes_namespace_v1.this.metadata[0].name
+    vault_role  = vault_kubernetes_auth_backend_role.this.role_name
+    secret_path = "secret/data/backstage-config" # checkov:skip=CKV_SECRET_6
+  }))
+}
