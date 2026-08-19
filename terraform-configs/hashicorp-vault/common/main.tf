c83dfb1be71e8683fe45a4199785ba51a598d764
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-19 17:04:45 +0700

    Fix cicd vault auth (#393)
    * prepare vault policy for backstge
    
    * enable kubernetes auth in hashicorp vault
    
    * Deploy secret provider for backstage
    
    * import cicd vault policy
    
    * import kubernetes vault qauth
    
    * Merge branch 'main' into fix-cicd-vault-auth
    
    * Merge branch 'main' into fix-cicd-vault-auth
    
    * Merge branch 'main' into fix-cicd-vault-auth

--- terraform-configs/hashicorp-vault/common/main.tf
+++ terraform-configs/hashicorp-vault/common/main.tf
@@ -1,3 +1,12 @@
 resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
+
+import {
+  to = vault_auth_backend.kubernetes
+  id = "kubernetes"
+}
+
+output "kubernetes_path" {
+  value = vault_auth_backend.kubernetes.path
+}
