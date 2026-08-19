ad96b6ddbfac60724e66c34d98a0928017c635b8
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-20 01:51:58 +0700

    skip invalid resources decalration for now

--- terraform-configs/kube-apps/backstage/main.tf
+++ terraform-configs/kube-apps/backstage/main.tf
@@ -41,12 +41,12 @@ resource "vault_kv_secret_v2" "backstage_config" {
   })
 }
 
-resource "kubernetes_manifest" "db_secrets" {
-  depends_on = [vault_kv_secret_v2.backstage_config]
+# resource "kubernetes_manifest" "db_secrets" {
+#   depends_on = [vault_kv_secret_v2.backstage_config]
 
-  manifest = yamldecode(templatefile("${path.module}/secret-provider.yaml", {
-    namespace   = kubernetes_namespace_v1.this.metadata[0].name
-    vault_role  = vault_kubernetes_auth_backend_role.this.role_name
-    secret_path = vault_kv_secret_v2.backstage_config.path
-  }))
-}
+#   manifest = yamldecode(templatefile("${path.module}/secret-provider.yaml", {
+#     namespace   = kubernetes_namespace_v1.this.metadata[0].name
+#     vault_role  = vault_kubernetes_auth_backend_role.this.role_name
+#     secret_path = vault_kv_secret_v2.backstage_config.path
+#   }))
+# }
