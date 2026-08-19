21e83f1fe7e6ecf020b5d76c4dfb85c9c8aa28d1
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-19 17:47:42 +0700

    prepare vault role for backstage

--- terraform-configs/kube-apps/backstage/data.tf
+++ terraform-configs/kube-apps/backstage/data.tf
@@ -1,3 +1,12 @@
+data "terraform_remote_state" "kubernetes_vault" {
+  backend = "consul"
+  config = {
+    address = "consul.laptop1.local"
+    scheme  = "http"
+    path    = "terraform-configs/hashicorp-vault/common"
+  }
+}
+
 data "vault_policy_document" "this" {
   rule {
     path         = "secret/data/backstage/*"
--- terraform-configs/kube-apps/backstage/main.tf
+++ terraform-configs/kube-apps/backstage/main.tf
@@ -17,18 +17,18 @@ resource "helm_release" "this" {
   ]
 }
 
-# resource "vault_kubernetes_auth_backend_role" "k8s_role" {
-#   backend                          = vault_auth_backend.kubernetes.path
-#   role_name                        = "k8s-app-role"
-#   bound_service_account_names      = ["webapp-sa"]
-#   bound_service_account_namespaces = ["production"]
-#   token_policies                   = ["webapp-policy"]
-#   token_ttl                        = 1800 # 30 minutes
-# }
-
 resource "vault_policy" "this" {
   name   = "backstage-jagat"
   policy = data.vault_policy_document.this.hcl
+}
+
+resource "vault_kubernetes_auth_backend_role" "this" {
+  backend                          = data.terraform_remote_state.kubernetes_vault.outputs.kubernetes_path
+  role_name                        = "backstage-app-role"
+  bound_service_account_names      = ["defaults"]
+  bound_service_account_namespaces = [kubernetes_namespace_v1.this.metadata[0].name]
+  token_policies                   = [vault_policy.this.name]
+  token_ttl                        = 1800 # 30 minutes
 }
 
 resource "kubernetes_manifest" "db_secrets" {
@@ -44,7 +44,7 @@ resource "kubernetes_manifest" "db_secrets" {
       provider: vault
       parameters:
         vaultAddress: "http://vault.laptop1.local"
-        roleName: "backstage-db-role"
+        roleName: ${vault_kubernetes_auth_backend_role.this.role_name}
         objects: |
           - objectName: "db_password"
             secretPath: "/secret/data/backstage/config"
