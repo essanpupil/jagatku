8f70536e04f841abf0ee143ed7dd2b4fb8b84506
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-19 22:35:51 +0700

    configure kubernetes auth (#395)

--- terraform-configs/hashicorp-vault/common/main.tf
+++ terraform-configs/hashicorp-vault/common/main.tf
@@ -10,3 +10,8 @@ import {
 output "kubernetes_path" {
   value = vault_auth_backend.kubernetes.path
 }
+
+resource "vault_kubernetes_auth_backend_config" "example" {
+  backend         = vault_auth_backend.kubernetes.path
+  kubernetes_host = "http://kubernetes.jagat.local"
+}
