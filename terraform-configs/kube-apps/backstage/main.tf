e2c5f35fce46b4a719b259ee2b5df79a4ce93e77
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-19 15:28:01 +0700

    prepare vault policy for backstge (#392)
    * prepare vault policy for backstge
    
    * enable kubernetes auth in hashicorp vault
    
    * Deploy secret provider for backstage

--- terraform-configs/kube-apps/backstage/main.tf
+++ terraform-configs/kube-apps/backstage/main.tf
@@ -16,3 +16,37 @@ resource "helm_release" "this" {
     file("${path.module}/values.yaml")
   ]
 }
+
+# resource "vault_kubernetes_auth_backend_role" "k8s_role" {
+#   backend                          = vault_auth_backend.kubernetes.path
+#   role_name                        = "k8s-app-role"
+#   bound_service_account_names      = ["webapp-sa"]
+#   bound_service_account_namespaces = ["production"]
+#   token_policies                   = ["webapp-policy"]
+#   token_ttl                        = 1800 # 30 minutes
+# }
+
+resource "kubernetes_manifest" "db_secrets" {
+  #checkov:skip=CKV_SECRET_6
+  #checkov:skip=APPSEC_SECRET_6
+  manifest = yamldecode(<<EOF
+    apiVersion: secrets-store.csi.x-k8s.io/v1
+    kind: SecretProviderClass
+    metadata:
+      name: backstage-db-auth
+      namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
+    spec:
+      provider: vault
+      parameters:
+        vaultAddress: "http://vault.laptop1.local"
+        roleName: "backstage-db-role"
+        objects: |
+          - objectName: "db_password"
+            secretPath: "/secret/data/backstage/config"
+            secretKey: "password"
+          - objectName: "db_username"
+            secretPath: "/secret/data/backstage/config"
+            secretKey: "username"
+  EOF
+  )
+}
