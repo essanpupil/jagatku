ec1b30ef77dfaf4143b9a346fa9dce13073450f3
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-19 23:01:57 +0700

    update deprecated resource

--- terraform-configs/hashicorp-vault/common/main.tf
+++ terraform-configs/hashicorp-vault/common/main.tf
@@ -11,7 +11,7 @@ output "kubernetes_path" {
   value = vault_auth_backend.kubernetes.path
 }
 
-data "kubernetes_config_map" "kube_root_ca" {
+data "kubernetes_config_map_v1" "kube_root_ca" {
   metadata {
     name      = "kube-root-ca.crt"
     namespace = "kube-system"
@@ -21,5 +21,5 @@ data "kubernetes_config_map" "kube_root_ca" {
 resource "vault_kubernetes_auth_backend_config" "example" {
   backend            = vault_auth_backend.kubernetes.path
   kubernetes_host    = "http://kubernetes.jagat.local"
-  kubernetes_ca_cert = data.kubernetes_config_map.kube_root_ca.data["ca.crt"]
+  kubernetes_ca_cert = data.kubernetes_config_map_v1.kube_root_ca.data["ca.crt"]
 }
