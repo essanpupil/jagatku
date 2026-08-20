6ed752d5be3253eec57e76d09f12ba83d7019b6a
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-21 01:40:07 +0700

    deploy vault connection in the same namespace with vault auth

--- terraform-configs/kube-apps/backstage/locals.tf
+++ terraform-configs/kube-apps/backstage/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  vault_policy_name    = "backstage-jagat"
-  secret_name          = "backstage-db-secret"
-  db_username          = "backstager"
-  service_account_name = "backstager-sa"
+  vault_policy_name     = "backstage-jagat"
+  secret_name           = "backstage-db-secret"
+  db_username           = "backstager"
+  service_account_name  = "backstager-sa"
+  vault_connection_name = "backstage-vault-conn"
 }
