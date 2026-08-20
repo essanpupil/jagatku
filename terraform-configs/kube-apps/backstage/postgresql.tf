9530a94cb506f8ebb72953a36e3dd7a7fe93ceae
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-20 11:58:24 +0700

    deploy cnpg object for backstage

---
+++ terraform-configs/kube-apps/backstage/postgresql.tf
@@ -0,0 +1,14 @@
+resource "kubernetes_manifest" "db_cluster" {
+  manifest = yamldecode(<<EOF
+        apiVersion: postgresql.cnpg.io/v1
+        kind: Cluster
+        metadata:
+            name: backstage-db-cluster
+            namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
+        spec:
+            instances: 3
+        storage:
+            size: 1Gi
+    EOF
+  )
+}
