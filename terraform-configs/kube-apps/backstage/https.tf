e4a678f39ea6d4a29b353e5b079dfba405970525
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-20 23:37:18 +0700

    try without issuer for pki secret backend (#416)
    * try without issuer for pki secret backend
    
    * Merge branch 'main' into fix-https-backstage

--- terraform-configs/kube-apps/backstage/https.tf
+++ terraform-configs/kube-apps/backstage/https.tf
@@ -12,9 +12,9 @@ resource "vault_pki_secret_backend_role" "intermediate_role" {
 }
 
 resource "vault_pki_secret_backend_cert" "backstage_jagatku_local" {
-  depends_on  = [vault_pki_secret_backend_role.intermediate_role]
-  issuer_ref  = data.terraform_remote_state.intermediate_ca.outputs.issuer_ref
-  backend     = data.terraform_remote_state.intermediate_ca.outputs.pki_path
+  depends_on = [vault_pki_secret_backend_role.intermediate_role]
+  # issuer_ref  = data.terraform_remote_state.intermediate_ca.outputs.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
   name        = vault_pki_secret_backend_role.intermediate_role.name
   common_name = "backstage.jagatku.local"
   ttl         = 3600
