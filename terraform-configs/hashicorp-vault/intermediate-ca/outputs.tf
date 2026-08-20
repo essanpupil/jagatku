60713a04d52cf00cbc2e0a714886c4fb61659666
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-20 18:29:10 +0700

    reconfigure intermedia ca in vault and deploy secret backend role for backstage (#411)

---
+++ terraform-configs/hashicorp-vault/intermediate-ca/outputs.tf
@@ -0,0 +1,7 @@
+output "pki_path" {
+  value = vault_mount.pki_int.path
+}
+
+output "issuer_ref" {
+  value = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+}
---
+++ terraform-configs/kube-apps/backstage/https.tf
@@ -0,0 +1,12 @@
+resource "vault_pki_secret_backend_role" "intermediate_role" {
+  backend          = data.terraform_remote_state.intermediate_ca.outputs.pki_path
+  issuer_ref       = data.terraform_remote_state.intermediate_ca.outputs.issuer_ref
+  name             = "backstage-jagatku-local"
+  ttl              = 86400
+  max_ttl          = 2592000
+  allow_ip_sans    = true
+  key_type         = "rsa"
+  key_bits         = 4096
+  allowed_domains  = ["jagatku.local"]
+  allow_subdomains = true
+}
