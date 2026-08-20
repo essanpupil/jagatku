e8f5ee4afefd0c5f9ffea33a018880cc341fe9d2
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-20 23:28:04 +0700

    reconfigure intermediate ca cert for https

--- terraform-configs/hashicorp-vault/intermediate-ca/outputs.tf
+++ terraform-configs/hashicorp-vault/intermediate-ca/outputs.tf
@@ -2,6 +2,6 @@ output "pki_path" {
   value = vault_mount.pki_int.path
 }
 
-output "issuer_ref" {
-  value = vault_pki_secret_backend_issuer.intermediate.issuer_ref
-}
+# output "issuer_ref" {
+#   value = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+# }
