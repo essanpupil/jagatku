9472686c8305ad72197f472c054ccace85b68a82
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-21 00:45:25 +0700

    deploy vault secrets operator (#417)

--- terraform-configs/kubernetes/hashicorp-vault/main.tf
+++ terraform-configs/kubernetes/hashicorp-vault/main.tf
@@ -16,3 +16,13 @@ resource "helm_release" "this" {
     file("${path.module}/values.yaml")
   ]
 }
+
+resource "helm_release" "vso" {
+  name       = "vault-secrets-operator"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault-secrets-operator"
+  namespace  = kubernetes_namespace_v1.this.metadata[0].name
+  version    = "1.5.1"
+  atomic     = true
+  wait       = true
+}
