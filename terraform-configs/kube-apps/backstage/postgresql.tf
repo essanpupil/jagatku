1b93985404cd3d05c923cb1c6168d9aba71f9466
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-20 12:42:02 +0700

    Main (#403)
    * deploy cnpg object for backstage
    
    * add doublequote for terraform resource escape
    
    * hardcode namespace
    
    * revert namespace terraform reference and fix storage indentation schema
    
    * deploy postgresql db cluster object
    
    * match configuration for secrets and db

---
+++ terraform-configs/kube-apps/backstage/postgresql.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_manifest" "db_cluster" {
+  manifest = yamldecode(<<EOF
+        apiVersion: postgresql.cnpg.io/v1
+        kind: Cluster
+        metadata:
+            name: backstage-db-cluster
+            namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
+        spec:
+            instances: 3
+            bootstrap:
+                initdb:
+                    database: backstage_db
+                    owner: ${local.db_username}
+                    secret:
+                        name: ${local.secret_name}
+            storage:
+                size: 1Gi
+    EOF
+  )
+}
