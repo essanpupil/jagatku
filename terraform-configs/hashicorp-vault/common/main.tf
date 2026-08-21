67f769428bd2e0fed51b24a5dd0e8b0a69aaa451
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-21 12:57:50 +0700

    configure vault to use https scheme for kubernetes connection

--- terraform-configs/hashicorp-vault/common/main.tf
+++ terraform-configs/hashicorp-vault/common/main.tf
@@ -4,7 +4,7 @@ resource "vault_auth_backend" "kubernetes" {
 
 resource "vault_kubernetes_auth_backend_config" "example" {
   backend            = vault_auth_backend.kubernetes.path
-  kubernetes_host    = "http://kubernetes.jagat.local:6443"
+  kubernetes_host    = "https://kubernetes.jagat.local:6443"
   kubernetes_ca_cert = data.kubernetes_config_map_v1.kube_root_ca.data["ca.crt"]
 }
 
