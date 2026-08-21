9d257a5615a7a45a005325fae0966bb44df5a94d
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-21 16:49:25 +0700

    update vault static secret mount (#433)

--- terraform-configs/kube-apps/backstage/vault.tf
+++ terraform-configs/kube-apps/backstage/vault.tf
@@ -63,7 +63,7 @@ resource "kubernetes_manifest" "backstage_secret" {
       name: ${local.secret_name}-static
     spec:
       vaultAuthRef: ${local.vault_auth_name}
-      mount: kvv2
+      mount: ${data.terraform_remote_state.kubernetes_vault.outputs.kv_secret_path}
       type: kv-v2
       path: ${vault_kv_secret_v2.backstage_config.path}
       version: 2
