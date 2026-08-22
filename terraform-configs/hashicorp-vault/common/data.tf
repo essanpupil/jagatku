9b85fcc53e7800bba818b2cb97b7a2f4b2e025f1
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-22 17:10:46 +0700

    remove unused config

--- terraform-configs/hashicorp-vault/common/data.tf
+++
@@ -1,6 +0,0 @@
-data "kubernetes_config_map_v1" "kube_root_ca" {
-  metadata {
-    name      = "kube-root-ca.crt"
-    namespace = "kube-system"
-  }
-}
--- terraform-configs/hashicorp-vault/common/main.tf
+++ terraform-configs/hashicorp-vault/common/main.tf
@@ -10,10 +10,3 @@ resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
   path = "kubernetes"
 }
-
-resource "vault_kubernetes_auth_backend_config" "example" {
-  backend                = vault_auth_backend.kubernetes.path
-  kubernetes_host        = "https://kubernetes.jagat.local:6443"
-  kubernetes_ca_cert     = data.kubernetes_config_map_v1.kube_root_ca.data["ca.crt"]
-  disable_iss_validation = true
-}
