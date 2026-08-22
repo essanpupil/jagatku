09cbf531603dc85fbcbb3e85b2dba6578b1b8c08
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-22 17:49:49 +0700

    uninstall unused helm release to reduce workload

--- terraform-configs/kubernetes/secrets-store-csi-driver/main.tf
+++ terraform-configs/kubernetes/secrets-store-csi-driver/main.tf
@@ -1,11 +1,11 @@
-resource "helm_release" "this" {
-  name       = "secrets-store-csi-driver"
-  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
-  chart      = "secrets-store-csi-driver"
-  namespace  = "kube-system"
-  atomic     = true
-  wait       = true
-  values = [
-    file("${path.module}/values.yaml")
-  ]
-}
+# resource "helm_release" "this" {
+#   name       = "secrets-store-csi-driver"
+#   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+#   chart      = "secrets-store-csi-driver"
+#   namespace  = "kube-system"
+#   atomic     = true
+#   wait       = true
+#   values = [
+#     file("${path.module}/values.yaml")
+#   ]
+# }
