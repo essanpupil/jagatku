5dcdc96130e738de97a5bf675bde5305125db0e6
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-25 16:20:22 +0700

    request cert for backstage https (#443)

--- terraform-configs/kube-apps/backstage/https.tf
+++ terraform-configs/kube-apps/backstage/https.tf
@@ -1,6 +1,6 @@
 resource "vault_pki_secret_backend_role" "intermediate_role" {
-  backend = data.terraform_remote_state.intermediate_ca.outputs.pki_path
-  # issuer_ref       = data.terraform_remote_state.intermediate_ca.outputs.issuer_ref
+  backend          = data.terraform_remote_state.intermediate_ca.outputs.pki_path
+  issuer_ref       = data.terraform_remote_state.intermediate_ca.outputs.issuer_ref
   name             = "backstage-jagatku-local"
   ttl              = 86400
   max_ttl          = 2592000
@@ -11,12 +11,11 @@ resource "vault_pki_secret_backend_role" "intermediate_role" {
   allow_subdomains = true
 }
 
-# resource "vault_pki_secret_backend_cert" "backstage_jagatku_local" {
-#   depends_on = [vault_pki_secret_backend_role.intermediate_role]
-#   # issuer_ref  = data.terraform_remote_state.intermediate_ca.outputs.issuer_ref
-#   backend     = vault_pki_secret_backend_role.intermediate_role.backend
-#   name        = vault_pki_secret_backend_role.intermediate_role.name
-#   common_name = "backstage.jagatku.local"
-#   ttl         = 3600
-#   revoke      = true
-# }
+resource "vault_pki_secret_backend_cert" "backstage_jagatku_local" {
+  issuer_ref  = data.terraform_remote_state.intermediate_ca.outputs.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "backstage.jagatku.local"
+  ttl         = 3600
+  revoke      = true
+}
