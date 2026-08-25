d0b05a8d1cca378fe9a6c1ec2ee6d7f0d16ca91f
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-25 23:13:19 +0700

    deploy secret tls for backstage

--- terraform-configs/kube-apps/backstage/locals.tf
+++ terraform-configs/kube-apps/backstage/locals.tf
@@ -1,7 +1,9 @@
 locals {
-  vault_policy_name    = "backstage-jagat"
-  secret_name          = "backstage-db-secret"
-  db_username          = "backstager"
-  service_account_name = "backstager-sa"
-  vault_auth_name      = "vault-auth"
+  vault_policy_name          = "backstage-jagat"
+  secret_name                = "backstage-db-secret"
+  db_username                = "backstager"
+  service_account_name       = "backstager-sa"
+  service_account_token_name = "backstager-sa-token" # checkov:ignore:CKV_SECRET_6
+  vault_auth_name            = "vault-auth"
+  cert_issuer_name           = "backstage-cert-issuer"
 }
--- terraform-configs/kube-apps/backstage/main.tf
+++ terraform-configs/kube-apps/backstage/main.tf
@@ -37,7 +37,7 @@ resource "kubernetes_cluster_role_binding_v1" "backstage" {
 
 resource "kubernetes_secret_v1" "backstager_sa" {
   metadata {
-    name      = "${local.service_account_name}-token"
+    name      = local.service_account_token_name
     namespace = kubernetes_namespace_v1.this.metadata[0].name
     annotations = {
       "kubernetes.io/service-account.name" = local.service_account_name
