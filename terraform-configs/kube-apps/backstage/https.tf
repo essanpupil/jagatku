9a799756e8530dbaa982c6d516ee773bbbe68a55
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-25 23:19:23 +0700

    Issue cert (#446)
    * create service account and secret token for cert-manager
    
    * deploy secret tls for backstage
    
    * add namespace

--- terraform-configs/kube-apps/backstage/https.tf
+++ terraform-configs/kube-apps/backstage/https.tf
@@ -19,3 +19,43 @@ resource "vault_pki_secret_backend_cert" "backstage_jagatku_local" {
   ttl         = 3600
   revoke      = true
 }
+
+resource "kubernetes_manifest" "cert_issuer" {
+  manifest = yamldecode(<<EOF
+    apiVersion: cert-manager.io/v1
+    kind: Issuer
+    metadata:
+      name: ${local.cert_issuer_name}
+      namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
+    spec:
+      vault:
+        server: http://vault.laptop1.local
+        path: ${data.terraform_remote_state.intermediate_ca.outputs.pki_path}
+        auth:
+          kubernetes:
+            mountPath: ${data.terraform_remote_state.kubernetes_vault.outputs.kubernetes_path}
+            role: issuer
+            secretRef:
+              name: ${local.service_account_token_name}
+              key: token
+  EOF
+  )
+}
+
+resource "kubernetes_manifest" "backstage_cert" {
+  manifest = yamldecode(<<EOF
+    apiVersion: cert-manager.io/v1
+    kind: Certificate
+    metadata:
+      name: backstage-jagatku-local
+      namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
+    spec:
+      secretName: backstage-jagatku-local-tls
+      issuerRef:
+        name: ${local.cert_issuer_name}
+      commonName: backstage.jagatku.local
+      dnsNames:
+        - backstage.jagatku.local
+  EOF
+  )
+}
