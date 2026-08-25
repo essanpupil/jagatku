658a8edcd16b7a0b166069956a06f0239f7d5cbe
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-25 16:52:43 +0700

    Deploy cert manager

---
+++ terraform-configs/kubernetes/cert-manager/terraform.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "~>1.15.0"
+  backend "consul" {
+    address = "consul.laptop1.local"
+    scheme  = "http"
+    path    = "terraform-configs/kubernetes/cert-manager"
+  }
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.2.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
