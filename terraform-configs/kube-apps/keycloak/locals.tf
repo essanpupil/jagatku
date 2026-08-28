bebd7236e61cdea3cf1832710f76adcdba36561a
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-29 02:10:33 +0700

    change db username (#462)

--- terraform-configs/kube-apps/keycloak/locals.tf
+++ terraform-configs/kube-apps/keycloak/locals.tf
@@ -1,6 +1,6 @@
 locals {
   secret_name          = "keycloak-secrets"
-  db_username          = "dbkeyhandler"
+  db_username          = "keycloak-db-user"
   db_name              = "keycloak-db"
   vault_policy_name    = "keycloak-policy"
   service_account_name = "keycloak-sa"
