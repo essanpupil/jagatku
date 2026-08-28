06a73e0748c09ac05c4960e6ca0879c5d5814844
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-28 17:45:41 +0700

    prpeare to deploy keycloak (#454)

---
+++ terraform-configs/kube-apps/keycloak/terraform.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = "~>1.15.0"
+  backend "consul" {
+    address = "consul.laptop1.local"
+    scheme  = "http"
+    path    = "terraform-configs/kube-apps/keycloak"
+  }
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.2.0"
+    }
+
+    vault = {
+      source  = "hashicorp/vault"
+      version = "5.11"
+    }
+  }
+}
+
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "vault" {
+  address          = "http://vault.laptop1.local"
+  skip_child_token = true
+  auth_login_token_file {
+    filename = pathexpand("~/.vault_pass")
+  }
+}
