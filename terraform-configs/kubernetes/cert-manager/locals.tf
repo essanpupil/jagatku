9ce4b858d4a83edec060408e12e41dd87e142913
Ikhsan N. Rosyidin <ikhsan.rosyidin@gmail.com>
2026-08-29 19:34:53 +0700

    Use self create service account for cert-manager to have full controll for the audience token

--- terraform-configs/kubernetes/cert-manager/locals.tf
+++ terraform-configs/kubernetes/cert-manager/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  service_account_name        = "cert-manager"
+  service_account_name        = "cert-manager-sa"
   service_account_secret_name = "cert-manager-token"
   pki_role_name               = "cert-manager-pki-role"
   allowed_domains             = ["jagatku.local"]
--- terraform-configs/kubernetes/cert-manager/main.tf
+++ terraform-configs/kubernetes/cert-manager/main.tf
@@ -4,6 +4,13 @@ resource "kubernetes_namespace_v1" "this" {
   }
 }
 
+resource "kubernetes_service_account_v1" "this" {
+  metadata {
+    name      = local.service_account_name
+    namespace = kubernetes_namespace_v1.this.metadata[0].name
+  }
+}
+
 resource "helm_release" "this" {
   name       = "cert-manager"
   repository = "oci://quay.io/jetstack/charts"
@@ -14,43 +21,9 @@ resource "helm_release" "this" {
   wait       = true
   values = [
     templatefile("${path.module}/values.yaml", {
-      service_account_name : local.service_account_name
+      service_account_name : kubernetes_service_account_v1.this.metadata[0].name
     })
   ]
-}
-
-resource "kubernetes_role_v1" "example" {
-  metadata {
-    name      = local.kubernetes_vault_role
-    namespace = kubernetes_namespace_v1.this.metadata[0].name
-    labels = {
-      test = "cert-manager-vault-kube-auth"
-    }
-  }
-
-  rule {
-    api_groups     = [""]
-    resources      = ["serviceaccounts/token"]
-    resource_names = [local.service_account_name]
-    verbs          = ["create"]
-  }
-}
-
-resource "kubernetes_role_binding_v1" "example" {
-  metadata {
-    name      = "${local.kubernetes_vault_role}-binding"
-    namespace = kubernetes_namespace_v1.this.metadata[0].name
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "Role"
-    name      = local.kubernetes_vault_role
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = local.service_account_name
-    namespace = kubernetes_namespace_v1.this.metadata[0].name
-  }
 }
 
 resource "kubernetes_cluster_role_v1" "this" {
