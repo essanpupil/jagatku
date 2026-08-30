6a2628015d4a7e77bf41e1b32604acdbfaff3bb8
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-31 03:57:04 +0700

    deploy keycloak with cnpg Cluster

--- terraform-configs/kube-apps/keycloak/data.tf
+++ terraform-configs/kube-apps/keycloak/data.tf
@@ -7,15 +7,6 @@ data "terraform_remote_state" "vault_common" {
   }
 }
 
-data "terraform_remote_state" "vault_secret_operator" {
-  backend = "consul"
-  config = {
-    address = "consul.laptop1.local"
-    scheme  = "http"
-    path    = "terraform-configs/kubernetes/vault-secret-operator"
-  }
-}
-
 data "vault_policy_document" "this" {
   rule {
     path         = vault_kv_secret_v2.app_passwd.path
--- terraform-configs/kube-apps/keycloak/database.tf
+++ terraform-configs/kube-apps/keycloak/database.tf
@@ -95,50 +95,50 @@ resource "kubernetes_manifest" "keycloak_app_secret" {
   )
 }
 
-# resource "kubernetes_manifest" "cnfg" {
-#   manifest = yamldecode(<<EOF
-#     apiVersion: postgresql.cnpg.io/v1
-#     kind: Cluster
-#     metadata:
-#       name: ${local.db_cluster_name}
-#       namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
-#     spec:
-#       serviceAccountName: ${kubernetes_service_account_v1.keycloak_sa.metadata[0].name}
-#       instances: 3
-#       storage:
-#         size: 1Gi
-#       bootstrap:
-#         initdb:
-#           database: ${local.db_name}
-#           owner: ${local.db_username}
-#           secret:
-#             name: ${local.app_secret_name}
-#       enableSuperuserAccess: true
-#       superuserSecret:
-#         name: ${local.superuser_secret_name}
-#   EOF
-#   )
-# }
+resource "kubernetes_manifest" "cnfg" {
+  manifest = yamldecode(<<EOF
+    apiVersion: postgresql.cnpg.io/v1
+    kind: Cluster
+    metadata:
+      name: ${local.db_cluster_name}
+      namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
+    spec:
+      serviceAccountName: ${kubernetes_service_account_v1.keycloak_sa.metadata[0].name}
+      instances: 3
+      storage:
+        size: 1Gi
+      bootstrap:
+        initdb:
+          database: ${local.db_name}
+          owner: ${local.db_username}
+          secret:
+            name: ${local.app_secret_name}
+      enableSuperuserAccess: true
+      superuserSecret:
+        name: ${local.superuser_secret_name}
+  EOF
+  )
+}
 
-# resource "kubernetes_manifest" "keycloak_db_app_role" {
-#   manifest = yamldecode(<<EOF
-#     apiVersion: postgresql.cnpg.io/v1
-#     kind: DatabaseRole
-#     metadata:
-#       name: keycloak-app-role
-#       namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
-#     spec:
-#       cluster:
-#         name: ${local.db_cluster_name}
-#       name: ${local.db_username}
-#       login: true
-#       superuser: false
-#       createdb: true
-#       databaseRoleReclaimPolicy: delete
-#       inRoles:
-#         - pg_monitor
-#       passwordSecret:
-#         name: ${local.app_secret_name}
-#   EOF
-#   )
-# }
+resource "kubernetes_manifest" "keycloak_db_app_role" {
+  manifest = yamldecode(<<EOF
+    apiVersion: postgresql.cnpg.io/v1
+    kind: DatabaseRole
+    metadata:
+      name: keycloak-app-role
+      namespace: ${kubernetes_namespace_v1.this.metadata[0].name}
+    spec:
+      cluster:
+        name: ${local.db_cluster_name}
+      name: ${local.db_username}
+      login: true
+      superuser: false
+      createdb: true
+      databaseRoleReclaimPolicy: delete
+      inRoles:
+        - pg_monitor
+      passwordSecret:
+        name: ${local.app_secret_name}
+  EOF
+  )
+}
