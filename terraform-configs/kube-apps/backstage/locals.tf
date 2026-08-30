03464aa97b56af57a7e52d392214066be4fb7d79
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-31 05:45:58 +0700

    rename db cluster to force create new Cluster to change postgres uid and guid

--- terraform-configs/kube-apps/backstage/locals.tf
+++ terraform-configs/kube-apps/backstage/locals.tf
@@ -3,7 +3,7 @@ locals {
   secret_name          = "backstage-secrets"
   db_username          = "backstager"
   service_account_name = "backstage-sa"
-  db_cluster_name      = "db-backstage-cluster"
+  db_cluster_name      = "db-cluster-backstage"
   vault_auth_name      = "backstage-vault-auth"
   tls_secret_name      = "backstage-jagatku-local-tls"
 }
