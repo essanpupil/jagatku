402316ebe61a9183737629d5f05121c7d315a0fc
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-31 05:29:46 +0700

    do not create keycloak statefulset before db ready

--- terraform-configs/kube-apps/keycloak/main.tf
+++ terraform-configs/kube-apps/keycloak/main.tf
@@ -58,149 +58,149 @@ resource "kubernetes_service_v1" "keycloak_discovery" {
   }
 }
 
-resource "kubernetes_stateful_set_v1" "keycloak" {
-  metadata {
-    labels = {
-      app = "keycloak"
-    }
-    name      = "keycloak"
-    namespace = kubernetes_namespace_v1.this.metadata[0].name
-  }
+# resource "kubernetes_stateful_set_v1" "keycloak" {
+#   metadata {
+#     labels = {
+#       app = "keycloak"
+#     }
+#     name      = "keycloak"
+#     namespace = kubernetes_namespace_v1.this.metadata[0].name
+#   }
 
-  spec {
-    service_name = kubernetes_service_v1.keycloak_discovery.metadata[0].name
-    replicas     = "2"
+#   spec {
+#     service_name = kubernetes_service_v1.keycloak_discovery.metadata[0].name
+#     replicas     = "2"
 
-    selector {
-      match_labels = {
-        app = "keycloak"
-      }
-    }
+#     selector {
+#       match_labels = {
+#         app = "keycloak"
+#       }
+#     }
 
-    template {
-      metadata {
-        labels = {
-          app = "keycloak"
-        }
-      }
+#     template {
+#       metadata {
+#         labels = {
+#           app = "keycloak"
+#         }
+#       }
 
-      spec {
-        container {
-          name  = "keycloak"
-          image = "quay.io/keycloak/keycloak:26.7.2"
-          args  = ["start"]
+#       spec {
+#         container {
+#           name  = "keycloak"
+#           image = "quay.io/keycloak/keycloak:26.7.2"
+#           args  = ["start"]
 
-          port {
-            container_port = 8080
-            name           = "http"
-          }
-          port {
-            container_port = 7800
-            name           = "jgroups"
-          }
-          port {
-            container_port = 57800
-            name           = "jgroups-fd"
-          }
+#           port {
+#             container_port = 8080
+#             name           = "http"
+#           }
+#           port {
+#             container_port = 7800
+#             name           = "jgroups"
+#           }
+#           port {
+#             container_port = 57800
+#             name           = "jgroups-fd"
+#           }
 
-          env {
-            name  = "KC_BOOTSTRAP_ADMIN_USERNAME"
-            value = "admin"
-          }
-          env {
-            name  = "KC_BOOTSTRAP_ADMIN_PASSWORD"
-            value = "admin"
-          }
-          env {
-            name  = "KC_PROXY_HEADERS"
-            value = "xforwarded"
-          }
-          env {
-            name  = "KC_HTTP_ENABLED"
-            value = "true"
-          }
-          env {
-            name  = "KC_HOSTNAME_STRICT"
-            value = "false"
-          }
-          env {
-            name  = "KC_HEALTH_ENABLED"
-            value = "true"
-          }
-          env {
-            name  = "KC_CACHE"
-            value = "ispn"
-          }
-          env {
-            name = "POD_IP"
-            value_from {
-              field_ref {
-                field_path = "status.podIP"
-              }
-            }
-          }
-          env {
-            name  = "KC_CACHE_EMBEDDED_NETWORK_BIND_ADDRESS"
-            value = "$(POD_IP)"
-          }
-          env {
-            name  = "KC_DB_URL_DATABASE"
-            value = local.db_name
-          }
-          env {
-            name  = "KC_DB_URL_HOST"
-            value = "${local.db_cluster_name}-rw.${kubernetes_namespace_v1.this.metadata[0].name}.svc.cluster.local"
-          }
-          env {
-            name  = "KC_DB"
-            value = "postgres"
-          }
-          env {
-            name = "KC_DB_PASSWORD"
-            value_from {
-              secret_key_ref {
-                name = local.app_secret_name
-                key  = "password"
-              }
-            }
-          }
-          env {
-            name  = "KC_DB_USERNAME"
-            value = local.db_username
-          }
+#           env {
+#             name  = "KC_BOOTSTRAP_ADMIN_USERNAME"
+#             value = "admin"
+#           }
+#           env {
+#             name  = "KC_BOOTSTRAP_ADMIN_PASSWORD"
+#             value = "admin"
+#           }
+#           env {
+#             name  = "KC_PROXY_HEADERS"
+#             value = "xforwarded"
+#           }
+#           env {
+#             name  = "KC_HTTP_ENABLED"
+#             value = "true"
+#           }
+#           env {
+#             name  = "KC_HOSTNAME_STRICT"
+#             value = "false"
+#           }
+#           env {
+#             name  = "KC_HEALTH_ENABLED"
+#             value = "true"
+#           }
+#           env {
+#             name  = "KC_CACHE"
+#             value = "ispn"
+#           }
+#           env {
+#             name = "POD_IP"
+#             value_from {
+#               field_ref {
+#                 field_path = "status.podIP"
+#               }
+#             }
+#           }
+#           env {
+#             name  = "KC_CACHE_EMBEDDED_NETWORK_BIND_ADDRESS"
+#             value = "$(POD_IP)"
+#           }
+#           env {
+#             name  = "KC_DB_URL_DATABASE"
+#             value = local.db_name
+#           }
+#           env {
+#             name  = "KC_DB_URL_HOST"
+#             value = "${local.db_cluster_name}-rw.${kubernetes_namespace_v1.this.metadata[0].name}.svc.cluster.local"
+#           }
+#           env {
+#             name  = "KC_DB"
+#             value = "postgres"
+#           }
+#           env {
+#             name = "KC_DB_PASSWORD"
+#             value_from {
+#               secret_key_ref {
+#                 name = local.app_secret_name
+#                 key  = "password"
+#               }
+#             }
+#           }
+#           env {
+#             name  = "KC_DB_USERNAME"
+#             value = local.db_username
+#           }
 
-          startup_probe {
-            http_get {
-              path = "/health/started"
-              port = 9000
-            }
+#           startup_probe {
+#             http_get {
+#               path = "/health/started"
+#               port = 9000
+#             }
 
-            initial_delay_seconds = 30
-            timeout_seconds       = 30
-          }
+#             initial_delay_seconds = 30
+#             timeout_seconds       = 30
+#           }
 
-          readiness_probe {
-            http_get {
-              path = "/health/ready"
-              port = 9000
-            }
+#           readiness_probe {
+#             http_get {
+#               path = "/health/ready"
+#               port = 9000
+#             }
 
-            initial_delay_seconds = 30
-            timeout_seconds       = 30
-          }
+#             initial_delay_seconds = 30
+#             timeout_seconds       = 30
+#           }
 
-          liveness_probe {
-            http_get {
-              path   = "/health/live"
-              port   = 9090
-              scheme = "HTTPS"
-            }
+#           liveness_probe {
+#             http_get {
+#               path   = "/health/live"
+#               port   = 9090
+#               scheme = "HTTPS"
+#             }
 
-            initial_delay_seconds = 30
-            timeout_seconds       = 30
-          }
-        }
-      }
-    }
-  }
-}
+#             initial_delay_seconds = 30
+#             timeout_seconds       = 30
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
