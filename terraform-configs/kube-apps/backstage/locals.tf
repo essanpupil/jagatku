17bf1dff5d25c02fd6586e3d4bcf08d2cbc4c003
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-08-31 15:08:08 +0700

    rename db clsuter to create new cluster to overcome immutable configuration

--- terraform-configs/kube-apps/backstage/locals.tf
+++ terraform-configs/kube-apps/backstage/locals.tf
@@ -3,7 +3,7 @@ locals {
   secret_name          = "backstage-secrets"
   db_username          = "backstager"
   service_account_name = "backstage-sa"
-  db_cluster_name      = "db-cluster-backstage"
+  db_cluster_name      = "backstage-db-cluster-1"
   vault_auth_name      = "backstage-vault-auth"
   tls_secret_name      = "backstage-jagatku-local-tls"
 }
--- terraform-configs/kube-apps/keycloak/locals.tf
+++ terraform-configs/kube-apps/keycloak/locals.tf
@@ -6,5 +6,5 @@ locals {
   vault_policy_name     = "keycloak-policy"
   service_account_name  = "keycloak-sa"
   vault_auth_name       = "keycloak-vault-auth"
-  db_cluster_name       = "db-cluster-keycloak"
+  db_cluster_name       = "keycloak-db-cluster-1"
 }
