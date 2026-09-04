29a2c62b87ea29e703de8b50f77b42ff4d34892d
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-09-04 16:45:39 +0700

    Import backstage repo

--- terraform-configs/github/backstage/main.tf
+++ terraform-configs/github/backstage/main.tf
@@ -1,34 +1,10 @@
-#trivy:ignore:AVD-GIT-0001
-resource "github_repository" "this" {
-  name                   = "backstage"
-  allow_merge_commit     = false
-  allow_rebase_merge     = false
-  allow_squash_merge     = true
-  delete_branch_on_merge = true
-  has_issues             = true
-  has_projects           = true
-  has_wiki               = true
-
-  #checkov:skip=CKV_GIT_3
-  #checkov:skip=CKV_GIT_1
-}
-
-#trivy:ignore:AVD-GIT-0004
-resource "github_branch_protection" "main" {
-  #checkov:skip=CKV_GIT_6
-  #checkov:skip=CKV_GIT_5
-  repository_id  = github_repository.this.node_id
-  pattern        = "main"
-  enforce_admins = true
-}
-
-
-resource "github_repository_vulnerability_alerts" "this" {
-  repository = github_repository.this.name
-  enabled    = true
+module "repo" {
+  # checkov:skip=CKV_TF_1: I owned the remote repo, safe to use tag as ref
+  source          = "git::https://github.com/essanpupil/iac-modules.git//github/repository?ref=v0.0.2"
+  repository_name = "backstage"
 }
 
 import {
-  to = github_repository.this
+  to = module.repo.github_repository.this
   id = "backstage"
 }
