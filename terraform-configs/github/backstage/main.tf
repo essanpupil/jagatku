8d1f3fb18567ec7ae96b84fe8806c0fafa848c75
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-09-04 17:09:50 +0700

    Update module ref to get fixed github module
    source: https://github.com/integrations/terraform-provider-github/issues/876#issuecomment-1303790559
    
    Had this problem, scratched my head on it for an hour or two, but @serain pointed me in the right direction.
    
    The child module didn't have a `required_providers {}` block, so when `terraform init` performed module resolution, it would configure the `integrations/github` provider in the root module, and then bring up `hashicorp/github` in the child module.
    
    The child module of course wouldn't get the provider config because the providers don't match, leading to it thinking that the owner config was blank.
    
    To fix it, in your root module:
    ```hcl
    terraform {
      required_providers {
        github = {
          source  = "integrations/github"
        }
      }
    }
    
    provider "github" {
      owner = "YourOrgName"
      token = "YourToken" // Or use ENV
    }
    ```
    
    Then in the child module:
    ```hcl
    terraform {
      required_providers {
        github = {
          source  = "integrations/github"
        }
      }
    }
    ```

--- terraform-configs/github/backstage/main.tf
+++ terraform-configs/github/backstage/main.tf
@@ -1,6 +1,6 @@
 module "repo" {
   # checkov:skip=CKV_TF_1: I owned the remote repo, safe to use tag as ref
-  source          = "git::https://github.com/essanpupil/iac-modules.git//github/repository?ref=v0.0.2"
+  source          = "git::https://github.com/essanpupil/iac-modules.git//github/repository?ref=v0.0.2-1"
   repository_name = "backstage"
 }
 
