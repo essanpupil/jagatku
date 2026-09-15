4468502ee2314282d2345716fb3575deddc5a3dc
Ikhsan N. Rosyidin <ikhsan.rosyidin@gmail.com>
2026-09-16 02:04:01 +0700

    redeploy vault secret operator

--- terraform-configs/kubernetes/vault-secret-operator/locals.tf
+++ terraform-configs/kubernetes/vault-secret-operator/locals.tf
@@ -1,6 +1,6 @@
 locals {
   vault_namespace        = "vault"
-  vault_kube_sa_name     = "jagat-vault-sa"
-  vault_kube_policy_name = "jagat-kube-policy"
-  kube_vault_role        = "jagat-kube-role"
+  vault_kube_sa_name     = "sa-jagat-vault"
+  vault_kube_policy_name = "policy-jagat-kube"
+  kube_vault_role        = "role-jagat-kube"
 }
